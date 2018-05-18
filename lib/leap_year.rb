f8024a6966e0022c7ea3e952a2b7f56dda4882cf
Somayeh <mail.somayeh@gmail.com>
2018-05-18 17:39:42 +0100

    Refactor the code

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,7 +1,3 @@
 def leap_year?(year)
-  if (year % 400).zero? || (year % 4).zero? && !(year % 100).zero?
-    true
-  elsif (year % 100).zero? && !(year % 400).zero? || !(year % 4).zero?
-    false
-  end
+  (year % 400).zero? || (year % 4).zero? && !(year % 100).zero?
 end
