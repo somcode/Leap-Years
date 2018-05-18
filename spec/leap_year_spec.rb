44784fcfffc3286892808bbc690d79edb5ff081f
Somayeh <mail.somayeh@gmail.com>
2018-05-18 16:11:29 +0100

    Test failed for returning false

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -3,4 +3,8 @@ describe 'leap_year?' do
   it 'return true if year is divisible by 400' do
     expect(leap_year?(2000)).to eq true
   end
+
+  it 'return false if year is Not divisible by 400' do
+    expect(leap_year?(2017)).to eq false
+  end
 end
