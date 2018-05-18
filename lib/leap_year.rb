aca2afbb7e75d7db1be6bae6736f2b6c80f17bba
Somayeh <mail.somayeh@gmail.com>
2018-05-18 16:28:41 +0100

    Add new condition to method

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,7 +1,9 @@
 def leap_year?(year)
   if (year % 400).zero?
-   true
-  else (year % 100).zero? && !(year % 400).zero?
+    true
+  elsif (year % 100).zero? && !(year % 400).zero?
+    false
+  else !(year % 4).zero?
     false
   end
 end
