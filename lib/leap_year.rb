8c41bdb5cd7c63b351313cfd1e1424f60b7d0ef2
Somayeh <mail.somayeh@gmail.com>
2018-05-18 17:37:44 +0100

    Add new condition to method to pass the test

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,7 +1,7 @@
 def leap_year?(year)
-  if (year % 400).zero?
+  if (year % 400).zero? || (year % 4).zero? && !(year % 100).zero?
     true
-  else (year % 100).zero? && !(year % 400).zero? || !(year % 4).zero?
+  elsif (year % 100).zero? && !(year % 400).zero? || !(year % 4).zero?
     false
   end
 end
